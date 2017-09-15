7511f898850be6edaf5fcc1e4dadc9850786f5d0
John-Lin <linton.tw@gmail.com>
2017-09-15 23:52:57 +0800

    only match openvswitch socket file path

--- utils.go
+++ utils.go
@@ -7,10 +7,11 @@ import (
 
 func ovsdbUnixPath() string {
 	var path string
-	cmd := "ps aux | awk '{print $12}' | grep -E \"unix:.*sock\""
+	cmd := "ps aux | awk '{print $12}' | grep -Eo unix:.*openvswitch.*sock"
 	out, _ := exec.Command("sh", "-c", cmd).Output()
 	if len(out) != 0 {
-		path = strings.Split(string(out[:]), "unix:")[1]
+		m := strings.Split(string(out[:]), "unix:")
+		path = m[len(m)-1]
 	}
 	return path
 }
