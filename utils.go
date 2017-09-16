85c352a75d082726a0abc2103eb097575ff8eb92
John-Lin <linton.tw@gmail.com>
2017-09-16 20:06:34 +0800

    fix: if there is no ovsdb-server process return empty string

--- utils.go
+++ utils.go
@@ -12,7 +12,10 @@ func ovsdbUnixPath() string {
 	out, _ := exec.Command("sh", "-c", cmd).Output()
 	if len(out) != 0 {
 		str := string(out[:])
-		path = re.FindStringSubmatch(str)[1]
+		strSubMatch := re.FindStringSubmatch(str)
+		if len(strSubMatch) != 0 {
+			path = strSubMatch[1]
+		}
 	}
 	return path
 }
